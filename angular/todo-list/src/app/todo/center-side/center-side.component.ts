b660f221bc28fef84cd6816ac92dd1813a3f4efe
Tim Babaryka <timasky95@gmail.com>
2022-04-05 18:23:42 +0300

    feat:main page design

--- angular/todo-list/src/app/todo/center-side/center-side.component.ts
+++ angular/todo-list/src/app/todo/center-side/center-side.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-center-side',
   templateUrl: './center-side.component.html',
-  styleUrls: ['./center-side.component.scss']
+  styleUrls: ['./center-side.component.scss'],
 })
-export class CenterSideComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class CenterSideComponent {
+  doSomething() {
+    console.log('I am doing something!');
   }
-
 }
--- angular/todo-list/src/app/todo/todo.module.ts
+++ angular/todo-list/src/app/todo/todo.module.ts
@@ -6,6 +6,7 @@ import { AuthGuard } from '../auth/auth.guard';
 import { LeftSideComponent } from './left-side/left-side.component';
 import { CenterSideComponent } from './center-side/center-side.component';
 import { RightSideComponent } from './right-side/right-side.component';
+import { MatIconModule } from '@angular/material/icon';
 
 const routes: Routes = [
   {
@@ -16,7 +17,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [TodoComponent, LeftSideComponent, CenterSideComponent, RightSideComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  declarations: [
+    TodoComponent,
+    LeftSideComponent,
+    CenterSideComponent,
+    RightSideComponent,
+  ],
+  imports: [CommonModule, RouterModule.forChild(routes), MatIconModule],
 })
 export class TodoModule {}
