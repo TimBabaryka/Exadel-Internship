01a97d9e69de10b5df09458baf8a9a3eb8aba030
Tim Babaryka <timasky95@gmail.com>
2022-04-13 17:24:42 +0300

    feat: routes for clicks

--- angular/todo-list/src/app/app-routing.module.ts
+++ angular/todo-list/src/app/app-routing.module.ts
@@ -1,9 +1,28 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { AdminComponent } from './todo/center-side/admin/admin.component';
+import { CategoriesComponent } from './todo/center-side/categories/categories.component';
+import { StatisticComponent } from './todo/center-side/statistic/statistic.component';
+import { TransactionsComponent } from './todo/center-side/transactions/transactions.component';
+import { TodoComponent } from './todo/todo/todo.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/todo' },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/todo',
+  },
+  {
+    path: 'todo',
+    component: TodoComponent,
+    children: [
+      { path: 'categories', component: CategoriesComponent },
+      { path: 'statistic', component: StatisticComponent },
+      { path: 'admin', component: AdminComponent },
+      { path: 'transactions', component: TransactionsComponent },
+    ],
+  },
   {
     path: '**',
     component: NotFoundComponent,
